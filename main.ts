166f66f5b1bf6a5d3363bfd0f2b51fd6243e5795
gonzaloSanchezsanz <96122660+gonzaloSanchezsanz@users.noreply.github.com>
2021-12-14 13:56:58 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+basic.showNumber(7)
 basic.forever(function () {
 	
+})
+loops.everyInterval(300, function () {
+    basic.showNumber(909)
+})
+loops.everyInterval(30, function () {
+    basic.showString("hola me llamo gonzalo")
 })
